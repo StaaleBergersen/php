b214295f8e7d898336d43ed5defc55d1806e4acc
Staale Bergersen <staale.bergersen@telemarkfylke.no>
2025-02-10 19:43:20 +0100

    slettet

--- 10_01_CRUED_bilregister/footer.php
+++
@@ -1,6 +0,0 @@
-
-    <div>
-            <p>© 2025 Bilregister. Alle rettigheter reservert.</p>
-            <p>Laget for 2ita våren 2025</p>
-            <p>Informasjonteknologi - Porsgrunn VGS </p>
-    </div>
--- 10_CRUED_CRM/footer.php
+++
@@ -1,7 +0,0 @@
-
-    <div class="footer-container">
-            <p>© 2025 CRM System. Alle rettigheter reservert.</p><br>
-            <p>Laget for 2ita våren 2025</p><br>
-            <p>Informasjonteknologi</p><br>
-            <p>Porsgrunn VGS</p>
-    </div>
