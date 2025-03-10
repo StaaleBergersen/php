37f77ed97a8901037b0550bb47503cd139e0aca8
Staale Bergersen <staale.bergersen@telemarkfylke.no>
2025-03-10 12:10:31 +0100

    laget mappe

---
+++ 11_00_CRUED_Sport/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php
+        ?>            include 'meny.php';
+
+    </header>
+    <main>
+    </main>
+    <footer>
+        <p>CRUED Sport</p>
+    </footer>
+</body>
+</html>
